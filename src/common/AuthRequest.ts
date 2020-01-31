5b9bb2d77d0f1ddae74e2394f805087af3790f7a
Maciej Kopeć <dimitor115@gmail.com>
2020-01-31 16:25:45 +0100

    Fix build errors remove paginate

--- src/common/AuthRequest.ts
+++ src/common/AuthRequest.ts
@@ -1,6 +1,5 @@
 import { Request as ExpressRequest } from 'express'
-import { IUser } from 'models/UserModel'
 
 export default interface AuthRequest extends ExpressRequest {
-    user: IUser;
+    user: any;
 }
